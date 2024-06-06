2d928207ccdf3fa5d13eded5f290def549f1d498
JHutts1172 <jesshutts@gmail.com>
2024-06-05 20:04:38 -0500

    finished problem 1

---
+++ problems/01-variables.js
@@ -0,0 +1,49 @@
+/*
+Time to practice variables! Follow the comments for instructions. Be sure to
+test your work by running your code and the test specs!
+*/
+
+// 1. Declare a variable called `firstName` and assign it your first name
+//    as a string:
+
+let firstName = "Jess";
+
+
+// 2. Declare a variable called `lastName` and assign it your last name
+//    as a string:
+
+let lastName = "Hutts";
+
+
+// 3. Declare a variable called `age`:
+
+let age;
+
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What
+//    do you expect them to be when they get printed?
+
+console.log(firstName); //Jess
+console.log(lastName); //Hutts
+console.log(age); //undefined
+
+
+// 5. Assign the `age` variable to a number:
+
+age = 0;
+
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What
+//    do you expect them to be when they get printed?
+
+console.log(firstName); //Jess
+console.log(lastName); //Hutts
+console.log(age); //0 
+
+
+/******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
+const obj = {};
+try { obj.firstName = firstName; } catch {}
+try { obj.lastName = lastName; } catch {}
+try { obj.age = age; } catch {}
+module.exports = obj;
